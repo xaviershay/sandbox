93dc81b0f105e7ec8fc99a520e2793f9881b598a
Xavier Shay <contact@xaviershay.com>
2016-12-08 17:29:59 -0800

    Update westpac scraper, much simpler approach.

--- misc/westpac_scrape.rb
+++ misc/westpac_scrape.rb
@@ -1,18 +1,17 @@
-# A script to scrape Westpac Online pages behind the login wall. I want
-# to download my statements automatically! I can't get it working though
-# - login is all good, but I get redirected to the "No Cookies" page.
-#
-# Usage:
-#   ruby westpac_scrape.rb
-require 'rubygems'
+# A proof of concept approach for logging in to Westpac Online Banking
 require 'mechanize'
 require 'json'
-require 'logger'
-require 'johnson'
-require "highline/import"
+require 'highline'
 
-username = ask("Customer #: ")
-password = ask("Password:" ) { |q| q.echo = false }
+EAM_URL = 'https://banking.westpac.com.au/eam/servlet/getEamInterfaceData'
+SUBMIT_URL = 'https://banking.westpac.com.au/eam/servlet/AuthenticateHttpServlet'
+LOGIN_URL = 'https://banking.westpac.com.au/wbc/banking/handler?TAM_OP=login&logout=false'
+
+cli = HighLine.new
+
+username = cli.ask("Enter your username:  ")
+unencoded_password = cli.ask("Enter your password:  ") { |q| q.echo = "x" }
+unencoded_password.upcase!
 
 # The keypad buttons have different values on every page load
 # Mappings are included in a JSON object on the page - grep
@@ -21,47 +20,27 @@ def encode_password(p, key, map)
   p.split(//).map {|x| key[map[x].to_i].chr }.join
 end
 
-agent = Mechanize.new{|a| a.log = Logger.new(STDERR) }
-page = agent.get('https://online.westpac.com.au/esis/Login/SrvPage')
-form = page.forms.detect {|x| x.action == "https://online.westpac.com.au/esis/Login/SrvPage" }
+agent = Mechanize.new
 
-body = page.body
+json = JSON.parse(agent.get(EAM_URL).body)
 
-json = JSON.parse(body.grep(/var keypadDef/)[0].gsub(/^var keypadDef = /, '').gsub(/;\s*$/, ''))
-
-password = encode_password(password, json['malgm'], json['keys'])
-
-# This is all mostly done via JS
-puts json.inspect
-form.action = json['submitToUrl']
-form.username_temp = username
-form.username = username
-form.password_temp = password
-form.password = 'w%s*%s' % [password, json['halgm']]
-form.halgm = json['halgm']
-
-# A cookie is added via JS, unsure if it is required
-def createWestpacIDCookie
-  # This JS is from the ui.js, run through JS beautifier and with the cookie checks removed
-  js = <<-JS
-    var d = new Date();
-    var b = new Date(d.getTime() + 365 * 24 * 60 * 60 * 1000 * 3);
-    var a = (Math.random() * 1000000);
-    CurrentYear = d.getYear();
-    if (CurrentYear < 1000) {
-        CurrentYear = CurrentYear + 1900
-    }
-    c = "d" + CurrentYear + (d.getMonth() + 1) + d.getDate() + "t" + d.getHours() + d.getMinutes() + d.getSeconds() + d.getTime() + "r" + Math.floor(a);
-    "WestpacID=" + escape(c) + "; expires=" + b.toGMTString() + "; domain=.westpac.com.au;path=/"
-  JS
-
-  cookie = Mechanize::Cookie.parse(URI.parse('https://online.westpac.com.au/'), Johnson.evaluate(js))
+keymap = {}
+json["keymap"]['keys'].each do |key_mapping|
+  keymap.merge!(key_mapping)
 end
 
-cookie = createWestpacIDCookie[0]
-agent.cookie_jar.add(URI.parse('https://online.westpac.com.au/'), cookie) || raise("fail")
+page = agent.get(LOGIN_URL)
+form = page.forms.first
 
-# Currently redirects to the No Cookies page, don't know why :(
+
+password = encode_password(unencoded_password, json["keymap"]['malgm'], keymap)
+
+form.action = SUBMIT_URL
+form.username = username
+form.password = password
+form.add_field!("halgm", json["keymap"]['halgm'])
+
 page = agent.submit(form)
 
-pp page
+puts "Account details: #{page.search(".tf-account-detail").text}"
+puts "Account balance: #{page.search(".balance.current .balance").text}"
