88212310a374328c5f66c4a8613ffe5eeb1e151e
Xavier Shay <contact@xaviershay.com>
2023-01-01 11:57:52 +1100

    Missing civ6 processing update

--- civ6/process_all_in_data.sh
+++ civ6/process_all_in_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-IFS=$(echo -en "\n\b"); rm consolidated.csv; for x in $(find data/*.json ); do ruby timeline_to_csv.rb "$x" >> consolidated.csv; done
+IFS=$(echo -en "\n\b")
+echo "Game ID,Turn,Era,Type,Score,Score Cumulative" > consolidated.csv
+for x in $(find data/*.json ); do ruby timeline_to_csv.rb "$x" >> consolidated.csv; done
