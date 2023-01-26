9c0846cdfe6c45e190be935501fca6e0bfda7faf
Xavier Shay <contact@xaviershay.com>
2023-01-27 07:38:18 +1100

    Potionomics solver kinda works?

--- potionomics/solve.rb
+++ potionomics/solve.rb
@@ -1,4 +1,5 @@
 require 'algorithms'
+require 'set'
 
 Migamins = Data.define(:r, :g, :y, :b, :p) do
   def to_a
@@ -22,6 +23,11 @@ Migamins = Data.define(:r, :g, :y, :b, :p) do
   def self.empty
     Migamins.new(0, 0, 0, 0, 0)
   end
+
+  def inspect
+    "<" +
+    %w(R G Y B P).zip(to_a).select {|_, n| n > 0 }.map {|l, n| "#{l}#{n}" }.join(" ") + ">"
+  end
 end
 
 Cauldron = Data.define(:max_migamins, :max_ingredients) do
@@ -36,7 +42,13 @@ Mix = Data.define(:ingredients) do
 
   def initialize(args)
     ingredients = args.fetch(:ingredients)
-    @summed_ingredients = ingredients.reduce(Migamins.empty) {|x, y| x.add(y) }
+    @summed_ingredients =
+      ingredients
+      .reduce(Migamins.empty) {|y, (x, n)|
+        z = y
+        n.times { z = z.add(x) }
+        z
+      }
     @count = @summed_ingredients.count
     super
   end
@@ -55,45 +67,56 @@ Mix = Data.define(:ingredients) do
     summed_ingredients.to_a.map {|x| x.to_f / count }
   end
 
-  def add(other_ingredient)
-    self.class.new(ingredients + [other_ingredient])
+  def add(other)
+    self.class.new(ingredients.merge(other.ingredients) {|k, a, b| a + b })
   end
 
   def ingredient_count
-    ingredients.length
+    ingredients.values.sum
   end
 
   def self.singleton(ingredient)
-    new([ingredient])
+    new({ingredient => 1})
   end
 end
 
 ingredients = [
   Migamins.new(6,0,0,0,0),
+  Migamins.new(4,0,0,0,0),
   Migamins.new(0,6,0,0,0),
+  Migamins.new(0,4,0,0,0),
+  Migamins.new(0,0,0,18,0),
 ].map {|x| [x, 100] }.to_h
 
-c = Cauldron.new(115, 2)
+c = Cauldron.new(35, 16)
 
 heap = Containers::MaxHeap.new
+seen = Set.new
 target_ratios = [0.5, 0.5, 0, 0, 0]
 
 ingredients.each do |i, n|
   mix = Mix.singleton(i)
   heap.push mix.value(target_ratios), mix
+  seen.add(mix)
 end
 
+max_value = 0
 while mix = heap.pop
-  new_mixes =
-    ingredients.keys
-      .map {|i| mix.add(i) }
-      .select {|m| c.can_contain?(m) }
+  # puts "EVAL: #{mix} #{mix.summed_ingredients.inspect}"
+  new_mixes = ingredients.keys
+    .map {|i| mix.add(Mix.singleton(i)) }
+    .select {|m| !seen.include?(m) && c.can_contain?(m) }
 
   new_mixes.each do |m|
     heap.push m.value(target_ratios), m
+    seen.add(m)
   end
 
   if new_mixes.empty?
-    puts "CANDIDATE: #{mix.value(target_ratios)} #{mix.count} #{mix.ingredient_count} #{mix}"
+    v = mix.value(target_ratios)
+    if v > max_value
+      puts "CANDIDATE: #{mix.value(target_ratios)} #{mix.count} #{mix.ingredient_count} #{mix}"
+      max_value = v
+    end
   end
 end
