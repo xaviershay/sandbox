c49885afc265ed2f1bd4be59df664ed5aefb1623
Xavier Shay <contact@xaviershay.com>
2023-01-27 12:35:16 +1100

    Add ingredient names, bit easier to read.

--- potionomics/solve.rb
+++ potionomics/solve.rb
@@ -1,180 +1,28 @@
 require 'algorithms'
 require 'set'
 require 'csv'
-
-Stop = Data.define(:label, :min_value) do
-  def inspect
-    "<Stop '#{label}' #{min_value}>"
-  end
-end
-
-$stops = [
-  Stop.new("Minor 0", 0),
-  Stop.new("Minor 1", 10),
-  Stop.new("Minor 2", 20),
-  Stop.new("Minor 3", 30),
-  Stop.new("Minor 4", 40),
-  Stop.new("Minor 5", 50),
-  Stop.new("Common 0", 60),
-  Stop.new("Common 1", 75),
-  Stop.new("Common 2", 90),
-  Stop.new("Common 3", 105),
-  Stop.new("Common 4", 120),
-  Stop.new("Common 5", 135),
-  Stop.new("Common 5", 135),
-  Stop.new("Greater 0", 150),
-  Stop.new("Greater 1", 150 + 23 * 1),
-  Stop.new("Greater 3", 150 + 23 * 2),
-  Stop.new("Greater 4", 150 + 23 * 3),
-  Stop.new("Greater 5", 150 + 23 * 4),
-  Stop.new("Grand 0", 290),
-  Stop.new("Grand 1", 290 + 30 * 1),
-  Stop.new("Grand 2", 290 + 30 * 2),
-  Stop.new("Grand 3", 290 + 30 * 3),
-  Stop.new("Grand 4", 290 + 30 * 4),
-  Stop.new("Grand 5", 290 + 30 * 5),
-]
-
-def stop_index(value)
-  $stops.take_while {|stop| stop.min_value <= value }.length - 1
-end
-
-def stop_for(value)
-  $stops.take_while {|stop| stop.min_value <= value }.last
-end
-
-Migamins = Data.define(:r, :g, :y, :b, :p) do
-  def to_a
-    [r, g, y, b, p]
-  end
-
-  def count
-    r + g + y + b + p
-  end
-
-  def add(other)
-    self.class.new(
-      r + other.r,
-      g + other.g,
-      y + other.y,
-      b + other.b,
-      p + other.p,
-    )
-  end
-
-  def self.empty
-    Migamins.new(0, 0, 0, 0, 0)
-  end
-
-  def inspect
-    "<" +
-    %w(R G Y B P).zip(to_a).select {|_, n| n > 0 }.map {|l, n| "#{l}#{n}" }.join(" ") + ">"
-  end
-end
-
-Cauldron = Data.define(:max_migamins, :max_ingredients) do
-  def can_contain?(mix)
-    mix.ingredient_count <= max_ingredients && mix.count <= max_migamins
-  end
-end
-
-Mix = Data.define(:ingredients) do
-  attr_reader :count
-  attr_reader :summed_ingredients
-
-  def initialize(args)
-    ingredients = args.fetch(:ingredients)
-    summed_ingredients =
-      ingredients
-      .reduce(Migamins.empty) {|y, (x, n)|
-        z = y
-        n.times { z = z.add(x) }
-        z
-      }
-    @count = summed_ingredients.count
-    @summed_ingredients = summed_ingredients
-    super
-  end
-
-  def value(target_ratios)
-    e = err(target_ratios)
-    stop_modifier =
-      if e > 0.1
-        -1000
-      elsif e < 0.0000000001
-        1
-      else
-        2
-      end
-
-    stop = stop_index(count) + stop_modifier
-
-    if stop >= 0
-      ($stops[stop] || $stops.last).min_value
-    else
-      count * 0.1
-    end
-  end
-
-  def err(target_ratios)
-    ratios.zip(target_ratios).map {|x, y| (x - y).abs }.sum
-  end
-
-  def valid?(target_ratios)
-    err(target_ratios) <= 0.1
-  end
-
-  def ratios
-    summed_ingredients.to_a.map {|x| x.to_f / count }
-  end
-
-  def add(other)
-    self.class.new(ingredients.merge(other.ingredients) {|k, a, b| a + b })
-  end
-
-  def ingredient_count
-    ingredients.values.sum
-  end
-
-  def self.singleton(ingredient)
-    new({ingredient => 1})
-  end
-end
-
-ingredients = [
-  Migamins.new(6,0,0,0,0),
-  Migamins.new(4,0,0,0,0),
-  Migamins.new(0,6,0,0,0),
-  Migamins.new(0,4,0,0,0),
-  Migamins.new(0,0,0,18,0),
-]
-ingredients = [
-  Migamins.new(6,0,0,0,0),
-  Migamins.new(4,0,0,0,0),
-  Migamins.new(24,0,0,0,0),
-  Migamins.new(30,0,0,0,0),
-  Migamins.new(40,0,0,20,0),
-  Migamins.new(18,0,0,6,0),
-  Migamins.new(0,0,0,30,0),
-  Migamins.new(0,0,0,18,0),
-  Migamins.new(0,0,0,12,0),
-]
+require_relative "./types"
 
 target_ratios = [0.5, 0.5, 0, 0, 0] # Health Potion
 target_ratios = [0.5, 0.0, 0, 0.5, 0] # Ice Tonic
 target_ratios = [0.5, 0.0, 0.5, 0, 0] # Fire Tonic
 target_ratios = [0.5, 0.5, 0.0, 0, 0] # Health Tonic
-target_ratios = [0, 0.5, 0, 0.5, 0] # Thunder Tonic
 target_ratios = [0.5, 0.0, 0.25, 0.25, 0] # Poison Cure
+target_ratios = [0, 0.5, 0, 0.5, 0] # Thunder Tonic
 
 data = CSV.read('data/ingredients.csv', headers: true)
 ingredients = data.map do |row|
-  Migamins.new(
-    *[row[2], row[3], row[4], row[5], row[6]].map(&:to_i)
+  Ingredient.new(
+    row[0],
+    Migamins.new(
+      *[row[2], row[3], row[4], row[5], row[6]].map(&:to_i)
+    )
   )
 end
 
-ingredients.select! {|i|  !i.to_a.zip(target_ratios).any? {|i, r| i > 0 && r == 0 } }
+ingredients.select! {|i|
+  !i.migamins.to_a.zip(target_ratios).any? {|i, r| i > 0 && r == 0 }
+}
 
 ingredients = ingredients.map {|x| [x, 100] }.to_h
 
@@ -191,9 +39,12 @@ ingredients.each do |i, n|
 end
 
 puts "Ingredient list:"
-ingredients.each do |i|
-  puts "  #{i.inspect}"
+ingredients.each do |k, v|
+  puts "  #{v} * %-10s #{k.name}" % [
+    k.migamins.inspect
+  ]
 end
+puts
 puts "Solving for #{c}\n  with #{target_ratios}"
 puts
 max_value = 0
@@ -212,11 +63,13 @@ while mix = heap.pop
     v = mix.value(target_ratios)
     if v > max_value && mix.valid?(target_ratios)
       stop = stop_for(mix.value(target_ratios))
-      puts "CANDIDATE: Value=#{mix.count} Ingredients=#{mix.ingredient_count} stop=#{stop.label}"
+      puts "CANDIDATE: value=#{mix.count} ingredients=#{mix.ingredient_count} quality=#{stop.label}"
       puts "  #{mix.summed_ingredients.inspect}"
       puts
       mix.ingredients.each do |k, v|
-        puts "  #{v} * #{k.inspect}"
+        puts "  #{v} * %-10s #{k.name}" % [
+          k.migamins.inspect
+        ]
       end
       puts
       max_value = v
---
+++ potionomics/types.rb
@@ -0,0 +1,140 @@
+Stop = Data.define(:label, :min_value) do
+  def inspect
+    "<Stop '#{label}' #{min_value}>"
+  end
+end
+
+$stops = [
+  Stop.new("Minor 0", 0),
+  Stop.new("Minor 1", 10),
+  Stop.new("Minor 2", 20),
+  Stop.new("Minor 3", 30),
+  Stop.new("Minor 4", 40),
+  Stop.new("Minor 5", 50),
+  Stop.new("Common 0", 60),
+  Stop.new("Common 1", 75),
+  Stop.new("Common 2", 90),
+  Stop.new("Common 3", 105),
+  Stop.new("Common 4", 120),
+  Stop.new("Common 5", 135),
+  Stop.new("Common 5", 135),
+  Stop.new("Greater 0", 150),
+  Stop.new("Greater 1", 150 + 23 * 1),
+  Stop.new("Greater 3", 150 + 23 * 2),
+  Stop.new("Greater 4", 150 + 23 * 3),
+  Stop.new("Greater 5", 150 + 23 * 4),
+  Stop.new("Grand 0", 290),
+  Stop.new("Grand 1", 290 + 30 * 1),
+  Stop.new("Grand 2", 290 + 30 * 2),
+  Stop.new("Grand 3", 290 + 30 * 3),
+  Stop.new("Grand 4", 290 + 30 * 4),
+  Stop.new("Grand 5", 290 + 30 * 5),
+]
+
+def stop_index(value)
+  $stops.take_while {|stop| stop.min_value <= value }.length - 1
+end
+
+def stop_for(value)
+  $stops.take_while {|stop| stop.min_value <= value }.last
+end
+
+Migamins = Data.define(:r, :g, :y, :b, :p) do
+  def to_a
+    [r, g, y, b, p]
+  end
+
+  def count
+    r + g + y + b + p
+  end
+
+  def add(other)
+    self.class.new(
+      r + other.r,
+      g + other.g,
+      y + other.y,
+      b + other.b,
+      p + other.p,
+    )
+  end
+
+  def self.empty
+    Migamins.new(0, 0, 0, 0, 0)
+  end
+
+  def inspect
+    "<" +
+    %w(R G Y B P).zip(to_a).select {|_, n| n > 0 }.map {|l, n| "#{l}#{n}" }.join(" ") + ">"
+  end
+end
+
+Cauldron = Data.define(:max_migamins, :max_ingredients) do
+  def can_contain?(mix)
+    mix.ingredient_count <= max_ingredients && mix.count <= max_migamins
+  end
+end
+
+Mix = Data.define(:ingredients) do
+  attr_reader :count
+  attr_reader :summed_ingredients
+
+  def initialize(args)
+    ingredients = args.fetch(:ingredients)
+    summed_ingredients =
+      ingredients
+      .reduce(Migamins.empty) {|y, (x, n)|
+        z = y
+        n.times { z = z.add(x.migamins) }
+        z
+      }
+    @count = summed_ingredients.count
+    @summed_ingredients = summed_ingredients
+    super
+  end
+
+  def value(target_ratios)
+    e = err(target_ratios)
+    stop_modifier =
+      if e > 0.1
+        -1000
+      elsif e < 0.0000000001
+        1
+      else
+        2
+      end
+
+    stop = stop_index(count) + stop_modifier
+
+    if stop >= 0
+      ($stops[stop] || $stops.last).min_value
+    else
+      count * 0.1
+    end
+  end
+
+  def err(target_ratios)
+    ratios.zip(target_ratios).map {|x, y| (x - y).abs }.sum
+  end
+
+  def valid?(target_ratios)
+    err(target_ratios) <= 0.1
+  end
+
+  def ratios
+    summed_ingredients.to_a.map {|x| x.to_f / count }
+  end
+
+  def add(other)
+    self.class.new(ingredients.merge(other.ingredients) {|k, a, b| a + b })
+  end
+
+  def ingredient_count
+    ingredients.values.sum
+  end
+
+  def self.singleton(ingredient)
+    new({ingredient => 1})
+  end
+end
+
+Ingredient = Data.define(:name, :migamins)
